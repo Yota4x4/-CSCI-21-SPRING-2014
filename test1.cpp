ea3835cc4496dd3b864f3772105c8f8ad90602ff
OpenShift guest <blipkin4x4@gmail.com>
2014-01-27 12:43:17 -0500

    Newcpp

---
+++ test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string userName;
+    
+    cout << "Please Type Your Name: ";
+    cin >> userName;
+    
+    cout << "Hello " << userName << " !" << endl;
+    return 0;
+}
